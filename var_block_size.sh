cdd37e0a250541df155ef0dfb01f6d88fac7d6d2
quaternior <42770936+quaternior@users.noreply.github.com>
2024-11-18 17:10:56 +0900

    Update var_block_size.sh

--- var_block_size.sh
+++ var_block_size.sh
@@ -1,8 +1,12 @@
 
 BLOCK_SIZE_LIST=(8 16 32 64 128)
-for BLOCK_SIZE in "${BLOCK_SIZE_LIST[@]}"
+BATCH_SIZE_LIST=(1 2 4 8 16)
+for BATCH_SIZE in "${BATCH_SIZE_LIST[@]}"
 do
-    ARGS="--block-size $BLOCK_SIZE"
-    trap 'kill 0' SIGINT
-    python ex.py ${ARGS}
+    for BLOCK_SIZE in "${BLOCK_SIZE_LIST[@]}"
+    do
+        ARGS="--block-size $BLOCK_SIZE"
+        trap 'kill 0' SIGINT
+        python ex.py ${ARGS}
+    done
 done
